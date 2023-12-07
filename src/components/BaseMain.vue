2670410b610d1d62320992a462ba051258958fab
ombabadugunda <m.kryzh@gmail.com>
2023-12-07 17:06:00 +0200

    background

--- src/components/BaseMain.vue
+++ src/components/BaseMain.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="main">
-    <video autoplay muted loop playsinline poster="../assets/icons/map-pin.png" class="background">
+    <video muted loop playsinline poster="../assets/icons/map-pin.png" class="background" onclick="this.play()">
       <source loop="true" src="../assets/video/background.mp4" type="video/mp4">
     </video>
     <h1 class="title">ТРИВАЄ ЗБІР НА СКВОТ-ДРОН #6</h1>
