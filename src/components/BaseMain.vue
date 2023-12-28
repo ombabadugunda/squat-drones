b59003a2c1ee123dc9b1244e45107a6e036b8999
ombabadugunda <m.kryzh@gmail.com>
2023-12-28 14:54:38 +0200

    Forth

--- src/components/BaseMain.vue
+++ src/components/BaseMain.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="main">
     <img :src="background" class="background" alt="">
-    <h1 class="title">ТРИВАЄ ЗБІР НА СКВОТ-ДРОН #3</h1>
+    <h1 class="title">ТРИВАЄ ЗБІР НА СКВОТ-ДРОН #4</h1>
     <h2 class="amount"><span class="green">{{ numberWithCommas(totalAmount) }}₴</span> <span class="slash">/</span> <span class="red">{{ numberWithCommas(40000) }}₴</span></h2>
     <h1 class="subtitle">на літак-камідзе з 3кг вибухівки</h1>
     <div class="flight">
